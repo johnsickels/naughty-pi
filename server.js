a434e3c2b5fa0f1799ac943b4f584d7f68f5d7b8
John Sickels <johnwsickels@gmail.com>
2021-03-31 14:51:19 +0100

    init

---
+++ server.js
@@ -0,0 +1,43 @@
+const Gpio = require('pigpio').Gpio;
+
+const motor = new Gpio(10, {mode: Gpio.OUTPUT});
+
+//console.log('500')
+//motor.servoWrite(500)
+
+//console.log('moving to 2000')
+//motor.servoWrite(2000)
+
+//setTimeout(() => {
+//  console.log('1000')
+//  motor.servoWrite(1000);
+//}, 1000);
+
+//setTimeout(() => {
+//  console.log('500')
+//  motor.servoWrite(500)
+//}, 2000);
+
+let pulseWidth = 1000;
+let increment = 100;
+let ctr = 0;
+
+const naughtyInterval = setInterval(() => {
+  motor.servoWrite(pulseWidth);
+//  if (flag) {
+//    motor.servoWrite(2000);
+//  } else {
+//    motor.servoWrite(1000)
+//  }
+//  flag = !!flag
+  pulseWidth += increment;
+  ctr++
+  if (pulseWidth >= 2000) {
+    increment = -100;
+  } else if (pulseWidth <= 1000) {
+    increment = 100;
+  }
+  if (ctr >= 20) {
+    clearInterval(naughtyInterval);
+  }
+}, 50);
