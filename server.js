9499eaafbf59f8c67d0d41aae831f22345f6d657
johnSickels <46609057+johnsickels@users.noreply.github.com>
2021-04-10 16:34:46 -0400

    wip

--- server.js
+++ server.js
@@ -1,50 +1,27 @@
-const Gpio = require('pigpio').Gpio;
+const Gpio = require("pigpio").Gpio;
 
-const motor = new Gpio(10, {mode: Gpio.OUTPUT});
+const motor = new Gpio(10, { mode: Gpio.OUTPUT });
 
-//console.log('500')
-//motor.servoWrite(500)
+const mid = 1500;
+const max = 2500;
+const min = 500;
 
-//console.log('moving to 2000')
-//motor.servoWrite(2000)
+// start at zero
+motor.servoWrite(mid);
 
-//setTimeout(() => {
-//  console.log('1000')
-//  motor.servoWrite(1000);
-//}, 1000);
-
-//setTimeout(() => {
-//  console.log('500')
-//  motor.servoWrite(500)
-//}, 2000);
-
-let pulseWidth = 1000;
-let increment = 100;
-let ctr = 0;
-
-//const naughtyInterval = setInterval(() => {
-//  motor.servoWrite(pulseWidth);
-//  if (flag) {
-//    motor.servoWrite(2000);
-//  } else {
-//    motor.servoWrite(1000)
-//  }
-//  flag = !!flag
-//  pulseWidth += increment;
-//  ctr++
-//  if (pulseWidth >= 2500) {
-//    increment = -100;
-//  } else if (pulseWidth <= 1000) {
-//    increment = 100;
-//  }
-//  if (ctr >= 30) {
-//    clearInterval(naughtyInterval);
-//  }
-//}, 100);
-motor.servoWrite(1500)
+// go to 100 degrees
 setTimeout(() => {
-  motor.servoWrite(2500)
-}, 500);
-setTimeout(() => {
-  motor.servoWrite(500)
+  motor.servoWrite(max);
 }, 1000);
+
+// go back to mid
+setTimeout(() => {
+  motor.servoWrite(mid);
+}, 2000);
+
+// setTimeout(() => {
+//   motor.servoWrite(min);
+// }, 1500);
+// setTimeout(() => {
+//   motor.servoWrite(mid);
+// }, 2000);
