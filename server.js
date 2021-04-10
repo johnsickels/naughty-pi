152f2581d7e09da2c74ca9cfab67b2186daec478
John Sickels <johnwsickels@gmail.com>
2021-04-10 21:24:40 +0100

    servo control

--- server.js
+++ server.js
@@ -22,22 +22,29 @@ let pulseWidth = 1000;
 let increment = 100;
 let ctr = 0;
 
-const naughtyInterval = setInterval(() => {
-  motor.servoWrite(pulseWidth);
+//const naughtyInterval = setInterval(() => {
+//  motor.servoWrite(pulseWidth);
 //  if (flag) {
 //    motor.servoWrite(2000);
 //  } else {
 //    motor.servoWrite(1000)
 //  }
 //  flag = !!flag
-  pulseWidth += increment;
-  ctr++
-  if (pulseWidth >= 2000) {
-    increment = -100;
-  } else if (pulseWidth <= 1000) {
-    increment = 100;
-  }
-  if (ctr >= 20) {
-    clearInterval(naughtyInterval);
-  }
-}, 50);
+//  pulseWidth += increment;
+//  ctr++
+//  if (pulseWidth >= 2500) {
+//    increment = -100;
+//  } else if (pulseWidth <= 1000) {
+//    increment = 100;
+//  }
+//  if (ctr >= 30) {
+//    clearInterval(naughtyInterval);
+//  }
+//}, 100);
+motor.servoWrite(1500)
+setTimeout(() => {
+  motor.servoWrite(2500)
+}, 500);
+setTimeout(() => {
+  motor.servoWrite(500)
+}, 1000);
