4da58e8d7181e8634d7342d1926bdf7983164613
John Sickels <johnwsickels@gmail.com>
2021-08-14 08:55:58 -0400

    youtube service

--- server.js
+++ server.js
@@ -1,77 +1,45 @@
-const Gpio = require("pigpio").Gpio;
+const express = require('express');
+const path = require('path');
 
-const motor = new Gpio(13, { mode: Gpio.OUTPUT });
+const youtubeService = require('./youtubeService.js');
 
-const mid = 1500;
-const max = 2500;
-const min = 500;
+const server = express();
 
-const sleep = (t) => {
-  return new Promise(function (resolve) {
-    setTimeout(function () {
-      resolve();
-    }, t);
-  });
-};
+server.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname + '/index.html'))
+);
 
-const main = async () => {
-  // start at mid
-  // move to max
-  for (let i = mid; i < max; i += 10) {
-    motor.servoWrite(i);
-    await sleep(10);
-  }
+server.get('/authorize', (request, response) => {
+  console.log('/auth');
+  youtubeService.getCode(response);
+});
 
-  // move to min
-  for (let i = max; i > min; i -= 10) {
-    motor.servoWrite(i);
-    await sleep(10);
-  }
+server.get('/callback', (req, response) => {
+  const { code } = req.query;
+  youtubeService.getTokensWithCode(code);
+  response.redirect('/');
+});
 
-  // move back to mid
-  for (let i = min; i < mid; i += 10) {
-    motor.servoWrite(i);
-    await sleep(10);
-  }
-};
+server.get('/find-active-chat', (req, res) => {
+  youtubeService.findActiveChat();
+  res.redirect('/');
+});
 
-main();
+server.get('/start-tracking-chat', (req, res) => {
+  youtubeService.startTrackingChat();
+  res.redirect('/');
+});
 
-// let pulseWidth = 1500;
-// let increment = 100;
-// let counter = 0;
+server.get('/stop-tracking-chat', (req, res) => {
+  youtubeService.stopTrackingChat();
+  res.redirect('/');
+});
 
-// const naughtyInterval = setInterval(() => {
-//   if (ctr > 40) {
-//     clearInterval(naughtyInterval);
-//   }
-//   motor.servoWrite(pulseWidth);
-//   ctr++;
+server.get('/insert-message', (req, res) => {
+  youtubeService.insertMessage("Hello World");
+  res.redirect('/');
+});
 
-//   pulseWidth += increment;
-//   if (pulseWidth >= max) {
-//     increment = -100;
-//   } else if (pulseWidth <= min) {
-//     increment = 100;
-//   }
-// }, 100);
-
-// go to 100 degrees
-// setTimeout(() => {
-//   motor.servoWrite(min);
-// }, 1000);
-
-// // go to 100 degrees
-// setTimeout(() => {
-//   motor.servoWrite(mid);
-// }, 2000);
-
-// // go back to mid
-// setTimeout(() => {
-//   motor.servoWrite(max);
-// }, 3000);
-
-// // go back to mid
-// setTimeout(() => {
-//   motor.servoWrite(mid);
-// }, 4000);
+server.listen(3000, function () {
+  console.log('Server is Ready');
+});
