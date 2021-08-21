9c1fc1a90bd1075b5df2cbf48814534cca3d24b6
John Sickels <johnsickels@gmail.com>
2021-08-21 19:15:04 +0100

    dates and colors in boot log

--- boot.sh
+++ boot.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
+LIGHT_PURPLE="\033[0;35m"
+NC="\033[0m"
+
+echo "${LIGHT_PURPLE}$(date)${NC}"
 echo "Waiting for GitHub server ..."
 sleep 60
 
+echo "${LIGHT_PURPLE}$(date)${NC}"
 echo 'pulling repo'
 cd /home/pi/naughty-pi && git pull
 
+echo "${LIGHT_PURPLE}$(date)${NC}"
 echo 'installing npm dependencies'
 npm i
 
+echo "${LIGHT_PURPLE}$(date)${NC}"
 echo 'done'
 
 exit 0
