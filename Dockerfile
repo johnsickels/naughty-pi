ae5410a3ad38d70cee0ae73660247d75aa8847c4
John Sickels <johnsickels@gmail.com>
2021-08-22 15:34:31 -0400

    new music, docker work

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,14 @@
 FROM alexellis2/streaming:07-05-2018
+
 COPY entry.sh entry.sh
-COPY lofi.m4a lofi.m4a
-COPY nyan.m4a nyan.m4a
+
+COPY lofi1.m4a lofi1.m4a
+COPY lofi2.m4a lofi2.m4a
+COPY lofi3.m4a lofi3.m4a
+COPY lofi4.m4a lofi4.m4a
+COPY lofi5.m4a lofi5.m4a
 COPY playlist.txt playlist.txt
+
 RUN chmod +x entry.sh
+
 ENTRYPOINT ["./entry.sh"]
--- entry.sh
+++ entry.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo Live-stream secret: $1
+echo Live-stream secret: "$STREAM_KEY"
 
-raspivid -o - -t 0 -w 1920 -h 1080 -fps 40 -b 8000000 -g 40 | ffmpeg -re -f concat -i playlist.txt -f h264 -i pipe:0 -c:v copy -c:a aac -ab 128k -g 40 -strict experimental -f flv -r 30 rtmp://a.rtmp.youtube.com/live2/$1
+raspivid -o - -t 0 -w 1920 -h 1080 -fps 40 -b 8000000 -g 40 | ffmpeg -re -f concat -i playlist.txt -f h264 -i pipe:0 -c:v copy -c:a aac -ab 128k -g 40 -strict experimental -f flv -r 30 rtmp://a.rtmp.youtube.com/live2/$STREAM_KEY
