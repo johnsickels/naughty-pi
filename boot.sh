78cde1128834553720e222c729dea0bcacfacc05
John Sickels <johnsickels@gmail.com>
2021-08-22 21:50:54 -0400

    shuffle, fresh build docker

--- boot.sh
+++ boot.sh
@@ -4,18 +4,42 @@ LIGHT_PURPLE="\033[0;35m"
 NC="\033[0m"
 
 echo "${LIGHT_PURPLE}$(date)${NC}"
-echo "Waiting for GitHub server ..."
+echo "Waiting for internet..."
 sleep 60
 
 echo "${LIGHT_PURPLE}$(date)${NC}"
-echo 'pulling repo'
+echo 'Pulling repo...'
 cd /home/pi/naughty-pi && git pull
 
 echo "${LIGHT_PURPLE}$(date)${NC}"
-echo 'installing npm dependencies'
+echo 'Installing npm dependencies...'
 npm i
 
 echo "${LIGHT_PURPLE}$(date)${NC}"
-echo 'done'
+echo "Shuffling playlist..."
+shuf playlist.txt --output=playlist.txt
+
+echo "${LIGHT_PURPLE}$(date)${NC}"
+echo "Cleaning up docker..."
+docker system prune -f
+
+echo "${LIGHT_PURPLE}$(date)${NC}"
+echo "Building docker..."
+npm run docker:build
+
+echo "${LIGHT_PURPLE}$(date)${NC}"
+echo "Starting docker container..."
+npm run docker:run
+
+echo "${LIGHT_PURPLE}$(date)${NC}"
+echo "Waiting for stream..."
+sleep 60
+
+echo "${LIGHT_PURPLE}$(date)${NC}"
+echo "Starting server..."
+npm start
+
+echo "${LIGHT_PURPLE}$(date)${NC}"
+echo 'Done!'
 
 exit 0
